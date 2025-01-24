bba4934c5851eb84e8cf13a64bf9512a25f66c52
abdulmajidcse <abdulmajidcse@gmail.com>
2025-01-24 23:19:29 +0600

    update header color and downdown.

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -21,22 +21,22 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="<?php echo url('/') ?>">Home</a>
+                                <a class="nav-link text-light" aria-current="page" href="<?php echo url('/') ?>">Home</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="<?php echo url('about') ?>">About</a>
+                                <a class="nav-link text-light" href="<?php echo url('about') ?>">About</a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
+                                <a class="nav-link text-light dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Admin
                                 </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li><a class="dropdown-item text-light" href="#">Profile</a></li>
+                                    <li><a class="dropdown-item text-light" href="#">Settings</a></li>
                                     <li>
                                         <hr class="dropdown-divider">
                                     </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    <li><a class="dropdown-item text-light" href="#">Logout</a></li>
                                 </ul>
                             </li>
                         </ul>
