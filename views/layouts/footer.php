fbe07052212882f6801cd950a275470e8e7cd894
abdulmajidcse <abdulmajidcse@gmail.com>
2025-01-24 12:00:29 +0600

    add footer container.

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,7 +1,9 @@
 </main>
 
 <footer class="bg-dark border-top border-body text-light text-center pt-3 pb-2">
-    <p>&copy; <?php echo date('Y') . ' ' . config('name') ?>. All rights are reserved.</p>
+    <div class="container">
+        <p>&copy; <?php echo date('Y') . ' ' . config('name') ?>. All rights are reserved.</p>
+    </div>
 </footer>
 
 <script src="<?php echo asset('js/bootstrap.bundle.min.js') ?>"></script>
