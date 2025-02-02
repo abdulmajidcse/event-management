c54d903afbdab643727571d7243102c3af1018b3
abdulmajidcse <abdulmajidcse@gmail.com>
2025-02-02 22:36:44 +0600

    remove ? sign for checking current uri.

--- app/helpers.php
+++ app/helpers.php
@@ -88,7 +88,7 @@ if (!function_exists('currentUri')) {
     function currentUri(): string
     {
         // get request uri
-        $queryString = !empty($_SERVER["QUERY_STRING"]) ? ('?' . $_SERVER["QUERY_STRING"]) : '';
+        $queryString = !empty($_SERVER["QUERY_STRING"]) ? ('?' . $_SERVER["QUERY_STRING"]) : '?';
         $uri = str_replace($queryString, '', $_SERVER["REQUEST_URI"]);
         // get server script name
         $scriptName = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
